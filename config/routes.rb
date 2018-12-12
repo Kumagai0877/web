556a307d0e0595bf884c8c552ec21fa1744a897e
熊谷洸希 <>
2018-12-12 15:13:07 +0900

    add sign out

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   get root :to => 'home#index'
+
+  # コールバック用URL
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  # ログアウト用
+  devise_scope :user do
+    delete :sign_out, to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 end
